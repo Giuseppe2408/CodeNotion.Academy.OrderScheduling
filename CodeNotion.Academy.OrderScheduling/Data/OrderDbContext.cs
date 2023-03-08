f22bf80c7dd6443bad4df225308680ccfd0c93e3
Giuseppe2408 <giuseppe.acito.ga@gmail.com>
2023-03-08 14:22:04 +0100

    initial commit

---
+++ CodeNotion.Academy.OrderScheduling/Data/OrderDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CodeNotion.Academy.OrderScheduling.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeNotion.Academy.OrderScheduling.Data;
+
+public class OrderDbContext : DbContext
+{
+    private DbSet<Order> Orders { get; set; }
+
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("data source=a6584a657b52;initial catalog=master;user id=sa;password=yourStrong(!)Password;TrustServerCertificate=True;");
+    }
+}
