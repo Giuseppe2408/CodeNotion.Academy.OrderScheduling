ccba86508a9fe8fe6ee80e90ec8c94591e160575
Giuseppe2408 <giuseppe.acito.ga@gmail.com>
2023-03-08 18:15:14 +0100

    api create e all funzionanti

--- CodeNotion.Academy.OrderScheduling/Data/OrderDbContext.cs
+++ CodeNotion.Academy.OrderScheduling/Data/OrderDbContext.cs
@@ -8,7 +8,7 @@ public class OrderDbContext : DbContext
     public DbSet<Order> Orders { get; set; }
 
 
-
+    //strina di connessione
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer("data source=a6584a657b52;initial catalog=master;user id=sa;password=yourStrong(!)Password;TrustServerCertificate=True;");
--- CodeNotion.Academy.OrderScheduling/Program.cs
+++ CodeNotion.Academy.OrderScheduling/Program.cs
@@ -1,11 +1,20 @@
+using System.Configuration;
+using CodeNotion.Academy.OrderScheduling.Data;
+using CodeNotion.Academy.OrderScheduling.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<OrderDbContext>();
+
+// add dependencies injection
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 
 var app = builder.Build();
 
