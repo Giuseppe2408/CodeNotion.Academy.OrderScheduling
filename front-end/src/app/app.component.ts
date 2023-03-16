456581484703d4a0fc1a9a63d135f30628c5d49d
Giuseppe2408 <giuseppe.acito.ga@gmail.com>
2023-03-16 16:29:51 +0100

    Subject in Observable

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { combineLatest, debounceTime, Observable, Observer, switchMap } from 'rxjs';
 import { Order, OrderClient } from './api.service';
 
 @Component({
@@ -8,58 +8,31 @@ import { Order, OrderClient } from './api.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'order-scheduling-angular';
-  order_list: Order[] = [];
-  filter_list: Order[] = [];
-  inputValueCustomer: any;
-  inputValueOrder: any;
-  data: Order[] = [];
-  constructor(private orderClient: OrderClient, private httpClient: HttpClient) {
-    orderClient.list().subscribe((list: Order[]) => this.order_list = list)
-    orderClient.list().subscribe((list: Order[]) => this.filter_list = list)
-  }
-
+  searchCustomer: string = "";
+  searchOrder: string = "";
   columnsToDisplay = ['id', 'customer', 'orderNumber', 'preparationDate', 'bendingDate', 'assemblyDate'];
 
-  applyFilterCustomer(event: Event) {
+  customerFilterObserver!: Observer<string>;
+  customerFilter$: Observable<string> = new Observable(observer => {
+    this.customerFilterObserver = observer;
+    observer.next()
+  })
+  orderFilterObserver!: Observer<string>;
+  orderFilter$: Observable<string> = new Observable(observer => {
+    this.orderFilterObserver = observer;
+    observer.next()
+  })
+  order$ = combineLatest([this.customerFilter$, this.orderFilter$]).pipe(
+    debounceTime(300),
+    switchMap(([customer, order]) => this.orderClient.list(customer ?? undefined, order ?? undefined))
+  )
 
-    this.httpClient.get("http://localhost:5181/api/Order/List", {
+  constructor(private orderClient: OrderClient) {}
 
-      params: {
-        customer: this.inputValueCustomer
-      },
-
-    }).subscribe(res => {
-
-      this.filter_list = Object.values(res)
-      this.filter_list = this.order_list.filter(or => or.customer?.includes(this.inputValueCustomer));
-
-    })
-
+  searchCustomerKeyUp() {
+    this.customerFilterObserver.next(this.searchCustomer)
   }
-
-  applyFilterOrder(event: Event) {
-
-    this.httpClient.get("http://localhost:5181/api/Order/List", {
-      
-      params: {
-        orderNumber: this.inputValueOrder
-      },
-
-    }).subscribe(res => {
-
-      this.filter_list = Object.values(res)
-      this.filter_list = this.order_list.filter(or => or.orderNumber?.includes(this.inputValueOrder));
-      
-    })
-
-  } 
-
+  searchOrderKeyUp() {
+    this.orderFilterObserver.next(this.searchOrder)
+  }
 }
-
-
-
-
-
-
-
